95cff3fe75568f4255f55cea687ddc9e071a1ad7
Philipp Neugebauer <ppn@nexgo.de>
2014-08-21 17:14:25 +0200

    main renamed to main

--- src/de/uniba/ppn/tananzeiger/Main.java
+++ src/de/uniba/ppn/tananzeiger/Main.java
@@ -2,7 +2,7 @@ package de.uniba.ppn.tananzeiger;
 
 import de.uniba.ppn.tananzeiger.controller.Controller;
 
-public class TanAnzeigerAnwendung {
+public class Main {
 	public static void main(String[] args) {
 		new Controller();
 	}
